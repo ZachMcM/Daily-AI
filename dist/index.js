d75fd61347cfb5969dce18379f1baaa008b194ad
Zach McMullen <zachmcmullen04@gmail.com>
2023-03-21 23:05:18 -0700

    dist file

---
+++ dist/index.js
@@ -0,0 +1,48 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const openai_1 = require("openai");
+const configuration = new openai_1.Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new openai_1.OpenAIApi(configuration);
+const twilio_1 = __importDefault(require("twilio"));
+const client = (0, twilio_1.default)(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const completion = yield openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "In a list style fashion give me a: random quote, a random history fact, and an idea for a programming project",
+        max_tokens: 2000
+    });
+    const response = completion.data.choices[0].text;
+    if (process.env.TO_NUM) {
+        client.messages.create({
+            body: response,
+            from: process.env.FROM_NUM,
+            to: process.env.TO_NUM
+        })
+            .then(message => console.log(message.sid));
+        res.json(`Message sent resposne was ${response}`);
+    }
+    else {
+        res.json('Error sending message');
+    }
+}));
+app.listen(8000 || process.env.PORT, () => {
+    console.log('App now running');
+});
